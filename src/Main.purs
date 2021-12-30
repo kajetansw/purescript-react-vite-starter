890048b1ff90a2d777340bf7bfce855049fce629
kswiatek <kajetan.swiatek@gmail.com>
2021-12-30 17:53:49 +0100

    moved Counter component to a separate module

--- src/Main.purs
+++ src/Main.purs
@@ -1,14 +1,12 @@
 module Main where
 
 import Prelude
+
+import App.Counter (mkCounter)
 import Data.Maybe (Maybe(..))
 import Effect (Effect)
 import Effect.Exception (throw)
 import React.Basic.DOM (render)
-import React.Basic.DOM as R
-import React.Basic.Events (handler_)
-import React.Basic.Hooks (Component, component, useState, (/\))
-import React.Basic.Hooks as React
 import Web.DOM.NonElementParentNode (getElementById)
 import Web.HTML (window)
 import Web.HTML.HTMLDocument (toNonElementParentNode)
@@ -22,33 +20,3 @@ main = do
   case container of
     Nothing -> throw "Could not find container element"
     Just c -> render (counter {}) c
-
-mkCounter :: Component {}
-mkCounter = do
-  component "Counter" \_ -> React.do
-    -- returns a tuple of current state and a setter function
-    -- initial state is passed as an argument
-    { count } /\ setCount <- useState { count: 0 }
-    -- renders a div with counter and two buttons
-    pure $ R.div
-      { children:
-          [ R.text $ "Count: " <> show count
-          , R.div
-              { className: "container"
-              , children:
-                  [ R.button
-                      { className: "button"
-                      , children: [ R.text "+" ]
-                      , onClick: handler_ do
-                          setCount \state -> { count: state.count + 1 }
-                      }
-                  , R.button
-                      { className: "button"
-                      , children: [ R.text "-" ]
-                      , onClick: handler_ do
-                          setCount \state -> { count: state.count - 1 }
-                      }
-                  ]
-              }
-          ]
-      }
---
+++ src/counter/Counter.purs
@@ -0,0 +1,38 @@
+module App.Counter (mkCounter) where
+
+import Prelude
+
+import React.Basic.DOM as R
+import React.Basic.Events (handler_)
+import React.Basic.Hooks (Component, component, useState, (/\))
+import React.Basic.Hooks as React
+
+mkCounter :: Component {}
+mkCounter = do
+  component "Counter" \_ -> React.do
+    -- returns a tuple of current state and a setter function
+    -- initial state is passed as an argument
+    { count } /\ setCount <- useState { count: 0 }
+    -- renders a div with counter and two buttons
+    pure $ R.div
+      { children:
+          [ R.text $ "Count: " <> show count
+          , R.div
+              { className: "container"
+              , children:
+                  [ R.button
+                      { className: "button"
+                      , children: [ R.text "+" ]
+                      , onClick: handler_ do
+                          setCount \state -> { count: state.count + 1 }
+                      }
+                  , R.button
+                      { className: "button"
+                      , children: [ R.text "-" ]
+                      , onClick: handler_ do
+                          setCount \state -> { count: state.count - 1 }
+                      }
+                  ]
+              }
+          ]
+      }
